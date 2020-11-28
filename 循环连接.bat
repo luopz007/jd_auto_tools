5dce994f4c054f64f3570162a2ef0767c2329f44
luopz007 <642002890@qq.com>
2020-11-28 09:59:44 +0800

    jd

--- 循环连接.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-title jd - connect
-:hello
-timeout /t 10
-adb connect 192.168.0.7
-cls
-goto hello
--- 看视频.bat
+++ 看视频.bat
@@ -2,6 +2,7 @@
 title jd - video
 :hello
 timeout /t 30
+adb shell input tap 350 1273
 adb shell input swipe 500 1000 500 50
 cls
 goto hello
